ac281cd217da517516c9ef15fc2710d87cacb41a
Anaira <ariana.quispep@pucp.edu.pe>
2020-06-06 21:56:34 -0500

    Nada

--- src/views/Configuracion/ViewInstitucion.vue
+++ src/views/Configuracion/ViewInstitucion.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="viewinstitucion">
     <CabezeraTitulo 
-    msg="Configuracion de la Institucion"
+    msg="Configuración de la Institución"
     :image-path="require('@/assets/images/0e37a827-396c-4641-8547-056f4b61bd98.jpg')"/>
     <FormInstitucion/>
   </div>
