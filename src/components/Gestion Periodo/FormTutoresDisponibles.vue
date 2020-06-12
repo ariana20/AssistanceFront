7d7c19a237ffcc154af5dd2eb524145c5b8144a1
Anaira <ariana.quispep@pucp.edu.pe>
2020-06-11 23:23:06 -0500

    Spin infinito :D

--- src/components/Gestion Periodo/FormTutoresDisponibles.vue
+++ src/components/Gestion Periodo/FormTutoresDisponibles.vue
@@ -8,6 +8,11 @@
         :tutor="item.tutor"
         :tipoTutoria="item.tipoTutoria" />
       </div>
+      <infinite-loading @infinite="infiniteHandler">
+        <span slot="no-more">
+          No hay mas datos
+        </span>
+      </infinite-loading>
 
     </div>
 
@@ -18,6 +23,7 @@
 
 import axios from 'axios'
 //import Swal from 'sweetalert2'
+//import InfiniteLoading from 'vue-infinite-loading'
 import datosTutor from '@/components/Gestion Periodo/DatosTutor.vue'
 
 export default {
@@ -47,20 +53,40 @@ export default {
       .post('/programa/tutores', params)
         .then(res =>{
           console.log(res.data);
-          this.tutores=res.data;            
+          this.tutores=res.data.tasks;            
         })
         .catch(e => {
           console.log(e.response);
         })
     },
-    /*
-    window.onscroll = () => {
-      let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
-
-      if (bottomOfWindow) {
-        // Do something, anything!
+    infiniteHandler: function($state){
+        let limit = this.tutores.length / 40 + 1;
+        const params = {
+          page: limit,
+          id : this.$store.state.programaActual.id_programa
+        };
+        axios
+        .post('/programa/tutores', params)
+          .then(res =>{
+            console.log(res.data);
+            this.loadMore($state, res);           
+          })
+          .catch(e => {
+            console.log(e.response);
+          })
+    },
+    loadMore: function($state, res){
+      if(res.data.tasks.length){
+        this.tutores=this.tutores.concat(res.data.tasks);
+        $state.loaded();
+        if(res.data.total==this.tutores.length){
+          $state.complete();
+        }
+      }else{
+        $state.complete();
       }
-    };*/
+
+    }
     
   }
 }
