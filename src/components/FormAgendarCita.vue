c4739749ac13a4fa340be5050c34ebb5ad198a3c
Jorge Leon <jrleon@pucp.edu.pe>
2020-06-13 20:38:16 -0400

    Revert

--- src/components/FormAgendarCita.vue
+++ src/components/FormAgendarCita.vue
@@ -2,25 +2,26 @@
     <div class="formagendarcita container">
         <div class="top-titulo" style="text-align:left;">
             <Fullcalendar ref="fullCalendar"
-                          :plugins ="calendarPlugins"
-                          defaultView ="timeGridWeek"
-                          :locales="locales"
+                          :plugins = "calendarPlugins"
+                          defaultView = "timeGridWeek"
+                          :locales= "locales"
                           locale="es"
-                          :header ="{
+                          :header = "{
                               left: 'title',
                               center: 'dayGridMonth, timeGridWeek, timeGridDay, listWeek',
                               right: 'prev today next'
                           }"
-                          :businessHours="businessHours"
-                          :columnHeaderFormat="columnFormat"
+                          :businessHours= "businessHours"
+                          :columnHeaderFormat= "columnFormat"
                           hiddenDays= [0]
                           :selectable="true"
-                          minTime="08:00:00"
-                          maxTime="22:00:00"
-                          :allDaySlot="false"
-                          :events ="EVENTS"
-                          @eventClick="handleClick"
-                          :customButtons="customButtons"
+                          minTime= "08:00:00"
+                          maxTime= "22:00:00"
+                          :allDaySlot= "false"
+                          :editable= "true"
+                          :events = "EVENTS"
+                          @select = "handleSelect"
+                          @eventClick= "handleClick"
                           />
             <modals-container/>
         </div>
@@ -45,50 +46,28 @@ import EventModal from './EventModal'
 export default {
     name: 'formAgendarCita',
     components: {Fullcalendar},
-    data () {
-        return {
-            calendarPlugins: [
-                DayGridPlugin,
-                TimeGridPlugin,
-                InteractionPlugin,
-                ListPlugin,
-                momentPlugin
-            ],
-            columnFormat: 'dddd',
-            calendar: null,
-            locales: [esLocale],
-            dispSemanalVistaAl: null,
-            businessHours: {
-                daysOfWeek: [ 1, 2, 3, 4, 5, 6],
-                startTime: '08:00', 
-                endTime: '22:00', 
-            },
-            customButtons: {
-                    prev: {
-                        text: 'custom prev !',
-                        click: () => {
-                            let calendar = this.$refs.fullCalendar.getApi();
-                            calendar.prev();
-                            this.getReminders();
-                        }
-                    },
-                    next: {
-                        text: 'custom ext!',
-                        click: () => {
-                            let calendar = this.$refs.fullCalendar.getApi();
-                            calendar.next();
-                            this.getReminders();
-                        }
-                    },
-            }
+    data: () => ({
+        calendarPlugins: [
+            DayGridPlugin,
+            TimeGridPlugin,
+            InteractionPlugin,
+            ListPlugin,
+            momentPlugin
+        ],
+        calendar: null,
+        locales: [esLocale],
+        dispSemanalVistaAl: null,
+        businessHours: {
+            daysOfWeek: [ 1, 2, 3, 4, 5, 6],
+            startTime: '08:00', 
+            endTime: '22:00', 
         }
-
-    },
+    }),
     computed: {
         ...mapGetters(["EVENTS"])
     },
     methods: {
-        /*handleSelect (arg) {
+        handleSelect (arg) {
             console.log(arg);
             this.$store.commit("ADD_EVENT", {
                 id: (new Date()).getTime(),
@@ -96,72 +75,28 @@ export default {
                 start: arg.start,
                 end: arg.end,
             });
-        },*/
+        },
         handleClick (arg) {
             this.$modal.show(EventModal,{
                 text: "This is from the component",
                 event: arg.event
-            });
-            
-        },
-        getReminders: function() {
-                this.calendar = this.$refs.fullCalendar.getApi();
-                this.$store.state.events = [];
-                axios.post('disponibilidades/dispSemanalVistaAl',{idUsuario:54,fechaIni:this.calendar.view.activeStart,fechaFin:this.calendar.view.activeEnd })
-                .then((response) => {
-                    //console.log('disp: ',response.data[0]);
-                    var rd = response.data[0];
-                    var rd2 = response.data[1];
-                    console.log(rd);
-                    console.log(rd2);
-                    for(var i in rd) {
-                        //this.events.push({
-                            if(rd2[i]=='o'){
-                                this.$store.commit("ADD_EVENT", {
-                                    id: rd[i].id_disponibildad,
-                                    title: 'Ocupado',
-                                    start: rd[i].fecha + " " + rd[i].hora_inicio,
-                                    type: rd[i].tipo_disponibilidad,
-                                    color: 'gray',
-                                });
-                            } else {
-                                this.$store.commit("ADD_EVENT", {
-                                    id: rd[i].id_disponibildad,
-                                    title: 'Libre',
-                                    start: rd[i].fecha + " " + rd[i].hora_inicio,
-                                    type: rd[i].tipo_disponibilidad,
-                                });
-                            }
-
-                        //});
-                    }
-                });
-                this.calendar.render();
-            },
-
-    },
-    watch: {    
-        eventFilter() {
-        this.$refs.calendar.fireMethod("rerenderEvents");
+            })
         }
     },
     mounted() {
-        this.getReminders();
-        //this.calendar = this.$refs.fullCalendar.getApi();
+        this.calendar = this.$refs.fullCalendar.getApi();
         //idUsuario: this.$store.state.usuario.id_usuario
-        //this.calendar.render();
-
-        /*axios.post('disponibilidades/dispSemanalVistaAl',{idUsuario:54,fechaIni:this.calendar.view.activeStart,fechaFin:this.calendar.view.activeEnd })
+        axios.post('disponibilidades/dispSemanalVistaAl',{idUsuario:50,fechaIni:this.calendar.view.activeStart,fechaFin:this.calendar.view.activeEnd })
         .then(response => {
             this.dispSemanalVistaAl = response.data;
+
             console.log(response.data);
         }).catch(e => {
             console.log(e.response);
-        });*/
+        });
     }
     
 }
-
 </script>
 
 <style lang='scss'>
@@ -201,9 +136,5 @@ export default {
 .fc-event { 
     background-color: #B2EBF2;
     border-color: #B2EBF2;
-}
-.vm--modal {
-    border-radius: 25px;
-    margin: 30px;
 }
 </style>
