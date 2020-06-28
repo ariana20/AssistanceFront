8bf2bc04ee29ae3de32f47f2e9ab4d299e1dfce9
Anaira <ariana.quispep@pucp.edu.pe>
2020-06-28 18:13:04 -0500

    dona y barras

---
+++ src/components/Reportes/DoughnutChart.vue
@@ -0,0 +1,37 @@
+<script>
+import {Doughnut} from "vue-chartjs";
+
+export default {
+    extends: Doughnut,
+    props:{
+        label:{
+            type:String
+        },
+        chartData:{
+            type:Array
+        },
+        options:{
+            type:Object
+        }
+    },
+    mounted(){
+        const dates = this.chartData.map(d=>d.date).reverse();
+        const totals = this.chartData.map(d=> d.total).reverse();
+
+        this.renderChart({
+           labels:dates,
+           datasets:[{
+                label:this.label,
+                data:totals,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+           }]
+
+        },
+        this.options
+        );
+    }
+    
+}
+</script>
