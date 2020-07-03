5bc3be78754d325030895538fe03cdfd74e60ff2
Anaira <ariana.quispep@pucp.edu.pe>
2020-07-02 20:47:48 -0500

    Horizontal Bar

---
+++ src/components/Reportes/HorizontalBarChart.vue
@@ -0,0 +1,44 @@
+<script>
+import {HorizontalBar} from "vue-chartjs";
+
+export default {
+    extends: HorizontalBar,
+    props:{
+        label:{
+            type:String
+        },
+        chartData:{
+            type:Array
+        },
+        options:{
+            type:Object
+        }
+    },
+    mounted(){
+        const dates = this.chartData.map(d=>d.date).reverse();
+        const totals = this.chartData.map(d=> d.total).reverse();
+
+        this.renderChart({
+           labels:dates,
+           datasets:[{
+                label:this.label,
+                data:totals,
+                // backgroundColor: ['rgba(255, 99, 132, 0.2)','rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)' ] ,
+                // borderColor: ['rgba(255, 99, 132, 1)','rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)'] ,
+                backgroundColor: ['rgba(255, 99, 132, 0.2)','rgba(75, 192, 192, 0.2)', 
+                                  'rgba(255, 99, 132, 0.5)','rgba(75, 192, 192, 0.5)',
+                                  'rgba(255, 99, 132, 1)','rgba(75, 192, 192, 1)' ] ,
+                borderColor: ['rgba(255, 99, 132, 1)','rgba(75, 192, 192, 1)', 
+                              'rgba(255, 99, 132, 1)','rgba(75, 192, 192, 1)',
+                              'rgba(255, 99, 132, 1)','rgba(75, 192, 192, 1)'] ,
+               
+                borderWidth: 1
+           }]
+
+        },
+        this.options
+        );
+    }
+    
+}
+</script>
