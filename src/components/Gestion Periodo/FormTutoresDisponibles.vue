b1f38f61e14ac36304f55d840739481d4faeb21f
Anaira <ariana.quispep@pucp.edu.pe>
2020-08-02 20:22:49 -0500

    nada creo e.e

--- src/components/Gestion Periodo/FormTutoresDisponibles.vue
+++ src/components/Gestion Periodo/FormTutoresDisponibles.vue
@@ -35,7 +35,7 @@
             <div class="col-12 col-lg-2" style="padding-top: 6px;"><h5>Temas: </h5></div>
             <div class="col-12 col-lg-8">
             <select class="form-control"  v-model="selectedTema" v-on:change="buscarTutor">
-                <option disabled selected :value="null" focusable="false">Selecciona un tema</option>
+                <!--option disabled selected :value="null" focusable="false">Selecciona un tema</!--option-->
                 <option 
                     v-for="(tema, index) in temas" 
                     :key="index" 
